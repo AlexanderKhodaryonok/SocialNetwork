388c87f20e0a43ebf3c098998bb90c83d6860285
Your name <alexanderkhodaryonok@gmail.com>
2019-02-18 12:38:25 +0300

    Extract from scr/ and delete .idea

--- src/Redux-BLL/axios/axios-instance.js
+++
@@ -1,8 +0,0 @@
-import * as axiosLib from "axios";
-
-const axios = axiosLib.create({
-    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
-    withCredentials: true
-});
-
-export default axios;
